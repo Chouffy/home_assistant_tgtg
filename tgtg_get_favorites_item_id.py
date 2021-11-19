2b7da0397b15a75c3721e524cfcd55f6d2d38d91
Hans-Paul Hemelaar <hans-paul.hemelaar@sogeti.com>
2021-11-19 20:14:53 +0100

    Change to mail loginwq

--- tgtg_get_favorites_item_id.py
+++ tgtg_get_favorites_item_id.py
@@ -2,9 +2,9 @@ from tgtg import TgtgClient
 # https://github.com/ahivert/tgtg-python
 
 email = "Your TGTG mail"
-password = "Your TGTG password"
 
-client = TgtgClient(email=email, password=password)
+client = TgtgClient(email=email)
+#Confirm the mail you receive. You have max 2 minutes
 
 # toto = client.get_item(item_id=529241)
 tgtgReply = client.get_items()
--- tgtg_get_tokens.py
+++ tgtg_get_tokens.py
@@ -1,15 +1,12 @@
 # Before running this, make sure that python, pip and tgtg are installed!
-# In a command line: pip install tgtg>=0.7.0
+# In a command line: pip install tgtg>=0.7.1 (waiting for merge request of tgtg)
 
 from tgtg import TgtgClient
 
-# ⚠ Change your email/password combo here.
-# If you don't have a password, try to reset it here: https://store.toogoodtogo.com/reset-password
 email = "Your TGTG mail"
-password = "Your TGTG password"
 
-# Set up a tgtg client
-client = TgtgClient(email=email, password=password)
+# Set up a tgtg client, and confirm the mail you received. You got 2 minutes
+client = TgtgClient(email=email)
 
 # Necessary to actually get the tokens
 client.get_items()
@@ -20,7 +17,6 @@ print("")
 print("sensor:")
 print("  - platform: tgtg")
 print("    username: '" + email + "'")
-print("    password: '" + password + "'")
 print("    access_token: '" + client.access_token + "'")
 print("    refresh_token: '" + client.refresh_token + "'")
 print("    user_id: '" + client.user_id + "'")
