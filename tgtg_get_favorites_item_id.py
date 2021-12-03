7da5084d53c93d24ea27d0dd6ac6b5657af0e9c4
Chouffy <git@chouffy.net>
2021-12-03 05:19:19 +0100

    Get favorites: Use the Get token script for login + some formatting

--- tgtg_get_favorites_item_id.py
+++ tgtg_get_favorites_item_id.py
@@ -1,17 +1,24 @@
-from tgtg import TgtgClient
-# https://github.com/ahivert/tgtg-python
+# Before running this, make sure that python3, pip and tgtg are installed!
+# In a command line: pip install tgtg>=0.10.0
 
-email = "Your TGTG mail"
-password = "Your TGTG password"
+from tgtg_get_tokens import tgtgClient
 
-client = TgtgClient(email=email, password=password)
+# The same client is re-used from the 1st script, so no need to rebuild it again
 
-# toto = client.get_item(item_id=529241)
-tgtgReply = client.get_items()
+# Below is if you want to get only one item_id information
+# tgtgReply = client.get_item(item_id=529241)
 
+# Get all favorites
+tgtgReply = tgtgClient.get_items()
+
+# Print them in a Home Assistant friendly way
+print("")
+print("Copy-paste this in your configuration.yaml file, inside the sensor / tgtg section:")
+print("")
 print('  item:')
 for item in tgtgReply:
     print('    #', item['display_name'])
     print('    - ', item['item']['item_id'])
 
+print("")
 input("Press enter to quit ...")
