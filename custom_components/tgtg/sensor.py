d5901be91602e2aab2c0b865ae3a2a021608497e
Arnoudd <126021001+Arnoudd@users.noreply.github.com>
2025-09-18 09:50:19 +0200

    Update sensor.py

--- custom_components/tgtg/sensor.py
+++ custom_components/tgtg/sensor.py
@@ -214,7 +214,7 @@ class TGTGSensor(SensorEntity):
         """
         global tgtg_client
         self.tgtg_answer = tgtg_client.get_item(item_id=self.item_id)
-        time.sleep(2)
+        time.sleep(1)
         self.tgtg_orders = tgtg_client.get_active()["orders"]
 
         self.store_name = self.tgtg_answer["display_name"]
